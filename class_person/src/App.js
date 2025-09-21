391f6b1f87ccb7fe2294e065ec13a507d83b5f8e
Mohanphideri <mohanphideri14@gmail.com>
2025-09-21 14:51:31 +0530

    add class _perosn

---
+++ class_person/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonHierarchy from './PersonHierarchy'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React - Person Class Hierarchy</h1>
+      <PersonHierarchy />  {/* render your component */}
+    </div>
+  );
+}
+
+export default App;
